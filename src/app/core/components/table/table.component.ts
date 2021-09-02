f154ac19b085543b85184cc8a7cfa5f8850a2805
ylavin <ylavin931221@gmail.com>
2021-09-02 11:25:44 -0400

    Added crud services for Team Entity

--- src/app/core/components/table/table.component.ts
+++ src/app/core/components/table/table.component.ts
@@ -8,6 +8,7 @@ import {TeamService} from "../../services/teamService";
 import {PlayerService} from "../../services/playerService";
 import {Team} from "../../models/team";
 import {League} from "../../models/league";
+import {Player} from "../../models/player";
 
 @Component({
   selector: 'app-table',
@@ -55,13 +56,8 @@ export class TableComponent implements OnInit {
   ngOnInit(): void {
     this.elements = this.dataTable;
     this.primengConfig.ripple = true;
-    this.teamService.getAllTeams().subscribe(respose => {
-      this.teams = respose as Team[]
-    })
-    this.leaguesService.getAllLeagues().subscribe(respose => {
-      this.leagues = respose as League[]
-    })
-
+    this.getAllTeams();
+    this.getAllLeagues();
   }
 
   openNew() {
@@ -91,14 +87,21 @@ export class TableComponent implements OnInit {
   }
 
   deleteElement(element: any) {
+    let isSuccess = false;
     this.confirmationService.confirm({
       message: '¿Estás seguro de deseas eliminar el elemento ' + element.name + '?',
       header: 'Confirmar',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.elements = this.elements.filter(val => val.id !== element.id);
-        this.element = {};
-        this.messageService.add({severity: 'success', summary: '', detail: 'Elemento eliminado', life: 3000});
+        if (this.title === 'Equipos') {
+          this.teamService.deleteById(element.id).subscribe(response => {
+            isSuccess = this.processResponse(response, 'delete');
+            if (isSuccess) {
+              this.getDataTableAfterSuccess();
+              this.element = {};
+            }
+          })
+        }
       }
     });
   }
@@ -110,32 +113,72 @@ export class TableComponent implements OnInit {
 
   saveElement() {
     this.submitted = true;
-
+    let isSuccess = false;
     if (this.element.name.trim()) {
       if (this.element.id) {
-        this.elements[this.findIndexById(this.element.id)] = this.element;
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Successful',
-          detail: 'Elemento actualizado satisfactoriamente',
-          life: 3000
-        });
+        if (this.title === 'Equipos') {
+          this.teamService.update(this.element, this.element.id).subscribe(response => {
+            isSuccess = this.processResponse(response, 'update');
+            if (isSuccess) {
+              this.getDataTableAfterSuccess();
+            }
+          });
+        }
       } else {
-        this.element.id = this.createId();
-        // this.product.image = 'product-placeholder.svg';
-        this.elements.push(this.element);
-        this.messageService.add({
-          severity: 'success',
-          summary: '',
-          detail: 'Elemento creado satisfactoriamente',
-          life: 3000
-        });
+        if (this.title === 'Equipos') {
+          this.element.id = this.createId();
+          this.teamService.create(this.element).subscribe(response => {
+            isSuccess = this.processResponse(response, 'create');
+            if (isSuccess) {
+              this.getDataTableAfterSuccess();
+            }
+          });
+        }
+        // this.element.id = this.createId();
+        // this.elements.push(this.element);
+        // this.messageService.add({
+        //   severity: 'success',
+        //   summary: '',
+        //   detail: 'Elemento creado satisfactoriamente',
+        //   life: 3000
+        // });
       }
 
       this.elements = [...this.elements];
       this.elementDialog = false;
       this.element = {};
     }
+  }
+
+  processResponse(res: any, type: string): boolean {
+    let isValid = true;
+    if (res && type === 'create') {
+      this.messageService.add({
+        severity: 'success',
+        summary: '',
+        detail: 'Elemento creado satisfactoriamente',
+        life: 3000
+      });
+    } else if (res && type === 'update') {
+      this.messageService.add({
+        severity: 'success',
+        summary: '',
+        detail: 'Elemento actualizado satisfactoriamente',
+        life: 3000
+      });
+    } else if (res && type === 'delete') {
+      this.messageService.add({severity: 'success', summary: '', detail: 'Elemento eliminado', life: 3000});
+
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: '',
+        detail: 'Ha ocurrido un error',
+        life: 3000
+      });
+      isValid = false;
+    }
+    return isValid
   }
 
   findIndexById(id: string): number {
@@ -182,6 +225,30 @@ export class TableComponent implements OnInit {
       options.push({label: value.name, value: value.id})
     });
     return options;
+  }
+
+  getAllTeams() {
+    this.teamService.getAllTeams().subscribe(response => {
+      this.teams = response as Team[]
+    });
+  }
+
+  getAllLeagues() {
+    this.leaguesService.getAllLeagues().subscribe(response => {
+      this.leagues = response as League[]
+    });
+  }
+
+  getDataTableAfterSuccess() {
+    if (this.title === 'Equipos') {
+      this.teamService.getAllTeams().subscribe(response => {
+        this.elements = response as Team[]
+      });
+    } else if (this.title === 'Jugadores') {
+      this.playerService.getAllPlayers().subscribe(response => {
+        this.elements = response as Player[]
+      });
+    }
   }
 }
 
--- src/app/core/services/teamService.ts
+++ src/app/core/services/teamService.ts
@@ -1,6 +1,7 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,49 @@ export class TeamService {
 
   getAllTeams() {
     return this.http.get(this.url + this.endpoint);
+  }
+
+  create(body: any) {
+    let response = null;
+    try {
+      response = this.http
+        .post(`${this.url}${this.endpoint}`, body);
+    } catch (error) {
+      response = this.errorHandler('POST', error);
+    }
+    return response;
+  }
+
+  public update(body: any, id: any): Observable<any> {
+    let response = null;
+    try {
+      response = this.http
+        .patch(`${this.url}${this.endpoint}/${id}`, body);
+    } catch (error) {
+      response = this.errorHandler('PATCH', error);
+    }
+    return response;
+  }
+
+  public deleteById(id: number | string): Observable<any> {
+    let response = null;
+    try {
+      response = this.http
+        .delete(`${this.url}${this.endpoint}/${id}`);
+    } catch (error) {
+      response = this.errorHandler('DELETE', error);
+    }
+    return response;
+  }
+
+  public errorHandler(
+    method: string,
+    error: HttpErrorResponse,
+  ): Observable<never> {
+    console.error(
+      `Error occurred during ${method} ${this.url}/${this.endpoint}`,
+      error,
+    );
+    return Observable.throw(error);
   }
 }
